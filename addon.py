e230826a61b63bb4721e47598a3d872942fb9cc0
leopheard <leopheard.misc@gmail.com>
2019-10-09 20:10:52 -0400

    upload

---
+++ addon.py
@@ -0,0 +1,46 @@
+from xbmcswift2 import Plugin, xbmcgui
+from resources.lib import mainaddon
+
+plugin = Plugin()
+url1 = "https://www.dreadcentral.com/category/podcasts/who-goes-there/feed/"
+url2 = "https://www.dreadcentral.com/feed/podcast/
+url3 = "http://www.dreadcentral.com/category/podcasts/horrible-imaginings/feed/"
+@plugin.route('/')
+def main_menu():
+    items = [
+        {
+            'label': plugin.get_string(30001), 
+            'path': plugin.url_for('episodes1'),
+            'thumbnail': "https://www.dreadcentral.com/wp-content/uploads/powerpress/TeethFinalTurq.png"},
+        {
+            'label': plugin.get_string(30002),
+            'path': plugin.url_for('episodes2'),
+            'thumbnail': "https://www.dreadcentral.com/wp-content/uploads/powerpress/DC_Podcast_Logo-120.jpg"},
+        {
+            'label': plugin.get_string(30003),
+            'path': plugin.url_for('episodes3'),
+            'thumbnail': "https://is4-ssl.mzstatic.com/image/thumb/Podcasts123/v4/63/79/ec/6379ec7a-6bdc-7d6e-cbf6-5cd36ab54322/mza_8839720560510624785.png/600x600bb.jpg"},
+    ]
+    return items
+
+@plugin.route('/episodes1/')
+def episodes1():
+    soup1 = mainaddon.get_soup1(url1)
+    playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
+    items = mainaddon.compile_playable_podcast1(playable_podcast1)
+    return items
+@plugin.route('/episodes2/')
+def episodes2():
+    soup2 = mainaddon.get_soup2(url2)
+    playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
+    items = mainaddon.compile_playable_podcast2(playable_podcast2)
+    return items
+@plugin.route('/episodes3/')
+def episodes3():
+    soup3 = mainaddon.get_soup3(url3)
+    playable_podcast3 = mainaddon.get_playable_podcast3(soup3)
+    items = mainaddon.compile_playable_podcast3(playable_podcast3)
+    return items
+
+if __name__ == '__main__':
+    plugin.run()
